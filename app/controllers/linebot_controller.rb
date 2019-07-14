0ae0bcf498b3b4a087dd6b794e4175ca8ff8a884
Noriya Saeki <noriya.saeki@gmail.com>
2019-07-15 01:28:04 +0900

    linebot_controller修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -4,7 +4,7 @@ class LinebotController < ApplicationController
   protect_from_forgery :except => [:callback]
 
   def client
-    @client ||= Line::Bot::Client.new {
+    @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
@@ -13,7 +13,7 @@ class LinebotController < ApplicationController
   def callback
     body = request.body.read
 
-    signature = request.env["HTTP_X_LINE_SIGNATURE"]
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       head :bad_request
     end
@@ -26,14 +26,14 @@ class LinebotController < ApplicationController
         case event.type
         when Line::Bot::Event::MessageType::Text
           message = {
-            type: 'text'
-            text: event.message["text"]
+            type: 'text',
+            text: event.message['text']
           }
-          client.reply_message(event["replyToken"], message)
+          client.reply_message(event['replyToken'], message)
         end
       end
     }
-
+    
     head :ok
   end
 end
