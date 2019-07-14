cf4fd8e2b00a227f6378c3f74750963671a517f2
Noriya Saeki <noriya.saeki@gmail.com>
2019-07-15 01:43:55 +0900

    おみくじbotためし

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -25,10 +25,13 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['#{text}ですか？']
-          }
+          # message = {
+          #   type: 'text',
+          #   text: event.message['text']
+          # }
+          if event.message['text'] =~ /おみくじ/
+            message[:text] = ["大吉", "中吉", "小吉", "凶", "大凶"].shuffle.first
+          end
           client.reply_message(event['replyToken'], message)
         end
       end
